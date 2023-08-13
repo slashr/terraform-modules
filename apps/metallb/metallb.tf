879626cb687359c901c621a0285bd8ed31d3c328
akash.nair <akash.nair@understand.ai>
2023-08-13 10:28:15 +0200

    Remove values file

--- apps/metallb/metallb.tf
+++ apps/metallb/metallb.tf
@@ -10,9 +10,8 @@ resource "helm_release" "metallb" {
   repository = "https://metallb.github.io/metallb"
   chart      = "metallb"
   version    = "~0.13.9"
-  values = [templatefile("${path.module}/values.yaml",{})]
 
   depends_on = [
     resource.kubernetes_namespace.metallb
-  ] 
+  ]
 }
